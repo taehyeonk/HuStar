f0bc35adad387b56fcbe7f8390a45ab8b00a8f55
TaeHyeon Kim <xogus9641@naver.com>
2021-05-13 10:42:35 +0900

    out 예약어 동작 예제

---
+++ C#(Refactoring)/Out/Out/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Out
+{
+    class Program
+    {
+        // return을 두 개를 받고 싶을 때 사용하는 것이 out
+        static int Sum(out float avg)
+        {
+            int iSum = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                iSum += i;
+            }
+            avg = iSum / 10.0f;
+            return iSum;
+        }
+        static void Main(string[] args)
+        {
+            float avg = 0.0f;
+            int iSum = Sum(out avg);
+            Console.WriteLine("Sum: {0}, Avg: {1}", iSum, avg);
+        }
+    }
+}
